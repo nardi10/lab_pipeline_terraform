170a8024621ba88cd02d5c41cb95e246f1b8fae0
Edvaldo Nardi <edvaldo.nardi@hotmai.com>
2025-06-11 19:18:34 -0300

    infra inicial de dev

--- main.tf
+++ main.tf
@@ -5,18 +5,14 @@ terraform {
     resource_group_name  = "Edvaldo"
     storage_account_name = "tfstatestoreed"
     container_name       = "tfstate"
-    key                  = "dev-1.tfstate" # Altere dinamicamente se necessário por branch
+    key                  = "teste.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
+
 
 resource "azurerm_resource_group" "example" {
   name     = "rg-projetoA-dev"
